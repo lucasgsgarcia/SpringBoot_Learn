01ecc73a1b11d7890657868909342e988e297b0a
Lucas Garcia <80185204+lucasgsgarcia@users.noreply.github.com>
2022-09-08 08:05:08 -0300

    correcao de diretórios

--- src/main/java/br/com/springboot/domain/repository/ClientesRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.springboot.domain.repository;
-
-import br.com.springboot.domain.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientesRepository extends JpaRepository<Cliente, Long> {
-    Cliente findById(long id);
-}
--- src/main/java/br/com/springboot/domain/repository/VendasRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.springboot.domain.repository;
-
-import br.com.springboot.domain.entity.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendasRepository extends JpaRepository<Venda, Long> {
-    Venda findById(long id);
-}
